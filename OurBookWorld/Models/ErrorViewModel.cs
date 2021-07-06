921b9e30772d50c53d657c7b8b7b48044ebaf720
irenazeiza <irena.zeiza@gmail.com>
2021-07-06 21:26:47 +0300

    ook

---
+++ OurBookWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OurBookWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
