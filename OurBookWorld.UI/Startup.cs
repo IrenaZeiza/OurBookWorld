d420a55a6aececdda8631f50205c33b86d194284
irena.zeiza@gmail.com <Daliena7>
2021-07-15 20:30:02 +0300

    ok

--- OurBookWorld.UI/Startup.cs
+++ OurBookWorld.UI/Startup.cs
@@ -50,7 +50,7 @@ namespace OurBookWorld.UI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=BookLib}/{action=Index}");
             });
         }
     }
