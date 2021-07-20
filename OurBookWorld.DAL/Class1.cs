fd6d648c885561bfc30f7e2c509d8f61e4933082
abriede <briedeagnes@gmail.com>
2021-07-20 18:50:18 +0300

    Commit all

--- OurBookWorld.DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace OurBookWorld.DAL
-{
-    public class Class1
-    {
-    }
-}
---
+++ OurBookWorld.UI/Controllers/UserInfoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurBookWorld.UI.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        public IActionResult UserContacts()
+        {
+            return View();
+        }
+
+    }
+}
