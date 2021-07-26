6c8d787db7f49c79a637e90b0753d729abf7d821
abriede <briedeagnes@gmail.com>
2021-07-26 21:04:18 +0300

    Added ReturnBook controller

---
+++ OurBookWorld.UI/Controllers/ReturnBookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurBookWorld.UI.Controllers
+{
+    public class ReturnBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
